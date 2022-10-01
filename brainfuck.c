28bd0859bccdcec746b920d4251fa1d204d81456
Eyre Turing <771778087@qq.com>
2022-10-01 20:27:42 +0800

    修改代码读入策略，去除结构体里的循环调用栈
    当有循环入口时，先把直到循环出口的代码读入，否则当出现一次也没执行的循环时会直接找不到循环出口
    
    循环的调用栈改为依赖函数里的临时变量，因为函数自己有栈的机制，利用了一下

--- brainfuck.c
+++ brainfuck.c
@@ -14,9 +14,7 @@ struct brainfuck_context {
 	char *code;
 	unsigned long long code_used;
 	unsigned long long code_size;
-	unsigned long long *call_stack;	// '[' positions
-	unsigned long long call_used;
-	unsigned long long call_size; 
+	unsigned long long end_depth;	// like [] or [][] is 1, [[]] is 2
 	unsigned long long code_pos;	// current code position to run
 };
 
@@ -25,7 +23,6 @@ static void brainfuck_delete(struct brainfuck_runner *runner)
 	free(runner->ctx->a_memory);
 	free(runner->ctx->b_memory);
 	free(runner->ctx->code);
-	free(runner->ctx->call_stack);
 	
 	free(runner->ctx);
 	
@@ -44,7 +41,7 @@ static int code_add(struct brainfuck_context *ctx, char c)
 		ctx->code = code;
 		ctx->code_size = (ctx->code_size + 1) * 2;
 	}
-	ctx->code_pos = ctx->code_used;
+	// ctx->code_pos = ctx->code_used;	// don't set code_pos here, because if c is [ need read until peer ]
 	ctx->code[(ctx->code_used)++] = c;
 	return 0;
 }
@@ -53,11 +50,11 @@ static int find_peer_end(struct brainfuck_context *ctx, unsigned long long *end)
 {
 	unsigned long long pos;
 	unsigned long long end_depth = 0;
-	for (pos = ctx->code_pos + 1; pos < ctx->code_used; ++pos) {
+	for (pos = ctx->code_pos; pos < ctx->code_used; ++pos) {
 		if (ctx->code[pos] == '[') {
 			++end_depth;
 		} else if (ctx->code[pos] == ']') {
-			if (end_depth-- == 0) {
+			if (--end_depth == 0) {
 				*end = pos;
 				return 0;
 			}
@@ -68,29 +65,12 @@ static int find_peer_end(struct brainfuck_context *ctx, unsigned long long *end)
 	return 1;
 }
 
-static int call_stack_add(struct brainfuck_context *ctx, unsigned long long pos)
-{
-	unsigned long long *stack_tmp = NULL;
-	if (ctx->call_size - ctx->call_used < 1) {
-		stack_tmp = (unsigned long long *)
-			realloc(ctx->call_stack, 
-			(ctx->call_size + 1) * 2 * sizeof(unsigned long long));
-		if (!stack_tmp) {
-			fprintf(stderr, "[kernel] expand call_stack failed!\n");
-			return 1;
-		}
-		ctx->call_stack = stack_tmp;
-		ctx->call_size = (ctx->call_size + 1) * 2;
-	}
-	ctx->call_stack[(ctx->call_used)++] = pos;
-	return 0;
-}
-
 static int code_run_tail(struct brainfuck_context *ctx)
 {
 	char *mem_tmp = NULL;
 	char ptrv;
 	unsigned long long peer_end_tmp;
+	unsigned long long pos_tmp;
 	int ret = 0;
 	switch(ctx->code[ctx->code_pos]) {
 	case '>':
@@ -155,12 +135,7 @@ static int code_run_tail(struct brainfuck_context *ctx)
 			printf("%c", ctx->a_memory[ctx->ptr]);
 		}
 		break;
-	case '[':
-		// add to call_stack
-		if (ret = call_stack_add(ctx, ctx->code_pos)) {
-			return ret;
-		}
-		
+	case '[':		
 		// check condition
 		if (ctx->ptr < 0) {
 			ptrv = ctx->b_memory[-(ctx->ptr) - 1];
@@ -170,29 +145,31 @@ static int code_run_tail(struct brainfuck_context *ctx)
 		
 		if (ptrv) {
 			// run body
-			for (++(ctx->code_pos); ctx->code_pos < ctx->code_used;
-				++(ctx->code_pos)) {
+			pos_tmp = ctx->code_pos;
+			++(ctx->code_pos);
+			while (ctx->code[ctx->code_pos] != ']') {
 				if (ret = code_run_tail(ctx)) {
 					return ret;
 				}
 			}
+			ctx->code_pos = pos_tmp;
+			ret = code_run_tail(ctx);
+			--(ctx->code_pos);
 		} else {
-			// move to peer ']' + 1 position
+			// move to peer ']' position
 			if (ret = find_peer_end(ctx, &peer_end_tmp)) {
 				return ret;
 			}
-			ctx->code_pos = peer_end_tmp + 1;
+			ctx->code_pos = peer_end_tmp;
 		}
 		break;
 	case ']':
-		if (!(ctx->call_used)) {
-			fprintf(stderr, "[kernel] loop failed!\n");
-			return 1;
-		}
-		ctx->code_pos = ctx->call_stack[--(ctx->call_used)];	// move and pop
-		ret = code_run_tail(ctx);
+		// because we will handle ] during [, aka, if enter this case, this ] is a single ], no [
+		fprintf(stderr, "[kernel] single ], [] used error, no [ found!\n");
+		return 1;
 		break;
 	}
+	++(ctx->code_pos);
 	return ret;
 }
 
@@ -202,6 +179,14 @@ static int brainfuck_add(struct brainfuck_runner *runner, char c)
 	
 	if (ret = code_add(runner->ctx, c)) {
 		return ret;
+	}
+	if (c == '[') {
+		++(runner->ctx->end_depth);
+	} else if (c == ']') {
+		--(runner->ctx->end_depth);
+	}
+	if (runner->ctx->end_depth) {	// scan loop code, do not run until get peer ]
+		return 0;
 	}
 	if (ret = code_run_tail(runner->ctx)) {
 		return ret;
@@ -246,9 +231,7 @@ struct brainfuck_runner *brainfuck_new()
 	ctx->code = NULL;
 	ctx->code_used = 0;
 	ctx->code_size = 0;
-	ctx->call_stack = NULL;
-	ctx->call_used = 0;
-	ctx->call_size = 0;
+	ctx->end_depth = 0;
 	ctx->code_pos = 0;
 	
 	runner->ctx = ctx;
