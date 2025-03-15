c6aab57bfed481fab106d1eb23e280dd3f104d75
Eyre-Turing <771778087@qq.com>
2025-03-15 14:15:28 +0800

    适配rpmbuild的buildroot不在默认路径时的rpmbuild打包

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -2,9 +2,12 @@
 
 cd "$(dirname "$0")" || exit 1
 
-mkdir -p ~/rpmbuild/SOURCES/bf-1.0
-cp -a . ~/rpmbuild/SOURCES/bf-1.0
-tar -zcf ~/rpmbuild/SOURCES/bf-1.0.tar.gz -C ~/rpmbuild/SOURCES/ bf-1.0
+SOURCES_DIR="$(dirname "$(dirname "$(rpmbuild --eval '%buildroot')")")/SOURCES"
+
+rm -rf "$SOURCES_DIR/bf-1.0"
+mkdir -p "$SOURCES_DIR/bf-1.0"
+cp -a * "$SOURCES_DIR/bf-1.0"
+tar -zcf "$SOURCES_DIR/bf-1.0.tar.gz" -C "$SOURCES_DIR/" bf-1.0
 rpmbuild -bb bf.spec
 
 
